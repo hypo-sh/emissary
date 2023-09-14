0d4a0066a611acaea9e2dd0f149776e2160c6eb6
Jake Zerrer <him@jakezerrer.com>
2023-09-14 12:10:20 -0400

    Move cljs tests

--- src/emissary/core.cljs
+++ src/emissary/core.cljs
@@ -1,6 +1,5 @@
 (ns emissary.core
-  (:require [clojure.string :refer [join]]
-            [hyperfiddle.rcf :as rcf]))
+  (:require [clojure.string :refer [join]]))
 
 (defn authorization-uri
   [{:keys [aud
@@ -19,27 +18,3 @@
                             (conj i (str k "=" v))) [] params))]
     ;; SECURITY: URL construction
     (str authorization-uri "?" params)))
-
-(rcf/tests
- "authorization-uri generates an appropriate URL"
-
- (authorization-uri
-  "https://localhost:8081"
-  {:aud "hypo"
-   :redirect-uri "https://www.hypo.sh/oauth_handler"
-   :scope ["oidc"]
-   :local-state {:name "jake"}
-   :response-type ["auth"]})
- :=
- "https://localhost:8081?client_id=hypo&local_state=%7B%3Aname%20%22jake%22%7D&redirect_uri=https%3A%2F%2Fwww.hypo.sh%2Foauth_handler&scope=oidc&response_type=auth")
-
-(defn redirect-to-authz-server
-  "4.1 step A"
-  [server config]
-  (let [path (authorization-uri server config)]
-    (set! (.. js/window -location -href) path)))
-
-;; 4.1 Step A
-
-;; TODO: Handle error, e.g.
-;; http://localhost:8080/redirect#error=unauthorized_client&error_description=Client+is+not+allowed+to+initiate+browser+login+with+given+response_type.+Implicit+flow+is+disabled+for+the+client.
---
+++ test/emissary/core_test.cljs
@@ -0,0 +1,22 @@
+(ns emissary.core-test
+  (:require [emissary.core :as sut]
+            [hyperfiddle.rcf :as rcf]))
+
+(rcf/tests
+ "authorization-uri generates an appropriate URL"
+
+ (sut/authorization-uri
+  "https://localhost:8081"
+  {:aud "hypo"
+   :redirect-uri "https://www.hypo.sh/oauth_handler"
+   :scope ["oidc"]
+   :local-state {:name "jake"}
+   :response-type ["auth"]})
+ :=
+ "https://localhost:8081?client_id=hypo&local_state=%7B%3Aname%20%22jake%22%7D&redirect_uri=https%3A%2F%2Fwww.hypo.sh%2Foauth_handler&scope=oidc&response_type=auth")
+
+(defn redirect-to-authz-server
+  "4.1 step A"
+  [server config]
+  (let [path (sut/authorization-uri server config)]
+    (set! (.. js/window -location -href) path)))
